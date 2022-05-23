d1422c6e104784dc2756155d0ff4c011f82f12d9
Leosousaaraujo <76224962+Leosousaaraujo@users.noreply.github.com>
2022-05-23 13:47:12 -0300

    Update simulador.js
    Remoção de variável global para armazenamento de cores, em conformidade com a atualização do input hidden

--- assets/simulador.js
+++ assets/simulador.js
@@ -1,8 +1,3 @@
-
-
-    globalColor = ""
-    
-
     /*Pega o clicado e renderiza*/
     $('img.min-ladrilhos').click(function() { 
         const svg = $(this).attr('src');
@@ -10,38 +5,27 @@
     });
 
     $('.picker').click(function() {
-        const color = $(this).val();
-        globalColor = $(this).val()
-        $("#selectedColor").val(color)
         $("#selectedColor").val( $(this).val() )
-        console.log(color)
-        console.log(globalColor)
     });
 
 
     /*Pintando*/
     $("#preview-ladrilho").on("click", ".A", function(){
-        $(".A").css("fill", globalColor )
         $(".A").css("fill", $("#selectedColor").val() )
     })
     $("#preview-ladrilho").on("click", ".B", function(){
-        $(".B").css("fill", globalColor )
         $(".B").css("fill", $("#selectedColor").val() )
     })
     $("#preview-ladrilho").on("click", ".C", function(){
-        $(".C").css("fill", globalColor )
         $(".C").css("fill", $("#selectedColor").val() )
     })
     $("#preview-ladrilho").on("click", ".D", function(){
-        $(".D").css("fill", globalColor )
         $(".D").css("fill", $("#selectedColor").val() )
     })
     $("#preview-ladrilho").on("click", ".E", function(){
-        $(".E").css("fill", globalColor )
         $(".E").css("fill", $("#selectedColor").val() )
     })
     $("#preview-ladrilho").on("click", ".F", function(){
-        $(".F").css("fill", globalColor )
         $(".F").css("fill", $("#selectedColor").val() )
     })
 
